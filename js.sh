ab71351d568720ccc1b7b6f155228e8758df32b0
pyknic <emil@speedment.com>
2017-10-01 16:33:52 -0700

    Don't depend on shaded artifact being present in .m2

--- js.sh
+++ js.sh
@@ -1,2 +1,2 @@
-jshell --class-path target/bridging-the-gap-1.0.0-SNAPSHOT.jar:~/.m2/repository/com/speedment/runtime-deploy/3.0.14/runtime-deploy-3.0.14.jar:~/.m2/repository/mysql/mysql-connector-java/5.1.42/mysql-connector-java-5.1.42.jar
-
+libs=`cat classpath`
+jshell --class-path target/bridging-the-gap-1.0.0-SNAPSHOT.jar:$libs
